16d91cb2fbcc122a92e4038fdbb7355f43934306
Joshua <Joshua.loran@gmail.com>
2019-05-21 11:56:09 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+console.log('Starting...')
+
+var WebSocketClient = require('websocket').client;
+var client = new WebSocketClient();
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
+
+client.on('connectFailed', function(error) {
+  console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+  console.log('WebSocket Client Connected');
+  connection.on('error', function(error) {
+      console.log("Connection Error: " + error.toString());
+  });
+  connection.on('close', function() {
+      console.log('Connection Closed');
+  });
+  connection.on('message', function(message) {
+      if (message.type === 'utf8') {
+          console.log("Received: '" + message.utf8Data + "'");
+      }
+  });
+  
+  function sendNumber() {
+      if (connection.connected) {
+          var number = Math.round(Math.random() * 0xFFFFFF);
+          connection.sendUTF(number);
+          setTimeout(sendNumber, 1000);
+      }
+  }
+  sendNumber();
+});
+
+function blinkLED() { //function to start blinking
+  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1); //set pin state to 1 (turn LED on)
+  } else {
+    LED.writeSync(0); //set pin state to 0 (turn LED off)
+  }
+}
+
+function endBlink() { //function to stop blinking
+  clearInterval(blinkInterval); // Stop blink intervals
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport GPIO to free resources
+}
+
+
+client.connect('wss://agile-reef-99245.herokuapp.com/cable');
+setTimeout(endBlink, 5000); //stop blinking after 5 seconds
